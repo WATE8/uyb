8172eb4ff2f02b409280b9ff234651d5b0617227
8NEMO <accidentxxxx@gmail.com>
2024-10-31 14:29:11 +0500

    яч я

--- src/main/java/searchengine/repositories/SiteRepository.java
+++ src/main/java/searchengine/repositories/SiteRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import searchengine.model.Site;
 
 public interface SiteRepository extends JpaRepository<Site, Long> {
+    // Здесь можно добавить дополнительные методы, если это необходимо
 }
